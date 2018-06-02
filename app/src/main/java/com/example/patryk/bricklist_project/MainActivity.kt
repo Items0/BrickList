35f77617e617e8f8076c80f8986dca6ac60ca0da
Items0 <nieznany012@wp.pl>
2018-06-02 15:23:48 +0200

    Create RecyclerView

--- app/src/main/java/com/example/patryk/bricklist_project/MainActivity.kt
+++ app/src/main/java/com/example/patryk/bricklist_project/MainActivity.kt
@@ -2,13 +2,43 @@ package com.example.patryk.bricklist_project
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    val myList = ArrayList<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        for (i in 0..20) myList.add("Set #$i")
+        viewManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+        viewAdapter = RecyclerViewAdapter(myList)
+
+
+        recyclerView = findViewById<RecyclerView>(R.id.myRecyclerView).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
+        }
+    }
+
+    fun addFunction(v: View) {
+        myList.add("Ends")
+        recyclerView.adapter.notifyItemInserted(myList.size - 1);
+        //recyclerView.adapter.notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/example/patryk/bricklist_project/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/patryk/bricklist_project/RecyclerViewAdapter.kt
@@ -3,18 +3,26 @@ package com.example.patryk.bricklist_project
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.view.View
+import android.widget.TextView
 
-class RecyclerViewAdapter(val items : ArrayList<String>) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+class RecyclerViewAdapter(val items : ArrayList<String>) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent?.context).inflate(R.layout.item_layout, parent, false)
+        return ViewHolder(v);
     }
 
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder?.myTextView?.text = items[position]
+    }
 
     override fun getItemCount(): Int {
         return items.size
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val myTextView = itemView.findViewById<TextView>(R.id.name)
     }
+
 }
