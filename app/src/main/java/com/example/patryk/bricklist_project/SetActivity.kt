62e506558834d0ee4e803a296fbdecd192fbf9ad
Items0 <nieznany012@wp.pl>
2018-06-05 03:10:40 +0200

    Create XML file to export

--- app/src/main/java/com/example/patryk/bricklist_project/SetActivity.kt
+++ app/src/main/java/com/example/patryk/bricklist_project/SetActivity.kt
@@ -5,8 +5,27 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.View
 import android.widget.LinearLayout
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_set.*
+import org.w3c.dom.Document
+import org.w3c.dom.Element
+import java.io.File
+import javax.xml.parsers.DocumentBuilder
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.transform.OutputKeys
+import javax.xml.transform.Transformer
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.stream.StreamResult
+import android.content.Intent
+import android.net.Uri
+import android.os.Environment
+import android.os.Environment.getExternalStorageDirectory
+
+
 
 class SetActivity : AppCompatActivity() {
 
@@ -57,4 +76,46 @@ class SetActivity : AppCompatActivity() {
         myDbHelper.modifyInventoriesParts(myList)
         super.finish()
     }
+
+    fun xmlExport(v: View) {
+        val docBuilder: DocumentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+        val doc: Document = docBuilder.newDocument()
+
+        val rootElement: Element = doc.createElement("INVENTORY")
+
+        Log.e("Size", myList.size.toString())
+        for (el in myList) {
+            Log.e("for", el.itemType + " " + el.id + " " + el.color + " " + el.qty)
+            if (el.qtyInStore != el.qty) {
+                val itemElement: Element = doc.createElement("ITEM")
+                val itemTypeElement: Element = doc.createElement("ITEMTYPE")
+                val itemIDElement: Element = doc.createElement("ITEMID")
+                val colorElement: Element = doc.createElement("COLOR")
+                val qtyfilledElement: Element = doc.createElement("QTYFILLED")
+
+                itemTypeElement.appendChild(doc.createTextNode(el.itemType))
+                itemIDElement.appendChild(doc.createTextNode(el.id))
+                colorElement.appendChild(doc.createTextNode(el.color))
+                qtyfilledElement.appendChild(doc.createTextNode((el.qty.toInt() - el.qtyInStore.toInt()).toString()))
+                itemElement.appendChild(itemTypeElement)
+                itemElement.appendChild(itemIDElement)
+                itemElement.appendChild(colorElement)
+                itemElement.appendChild(qtyfilledElement)
+                rootElement.appendChild(itemElement)
+            }
+        }
+        doc.appendChild(rootElement)
+        val transformer: Transformer = TransformerFactory.newInstance().newTransformer()
+
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes")
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2")
+
+        transformer.transform(DOMSource(doc), StreamResult(File("$filesDir/xml-test-output.xml")))
+
+        //Toast.makeText(this, "XML Complete  " + Environment.getExternalStorageDirectory().absolutePath, Toast.LENGTH_LONG).show()
+
+        
+
+    }
+
 }
